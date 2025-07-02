d7b8e6b64db05d31d5ea9959fb3687738b4d5617
N3nT <bar55v71@interia.pl>
2025-07-02 16:50:59 +0200

    Search algorithms

---
+++ search/README.md
@@ -0,0 +1,17 @@
+## Linear search
+Time complexity: O(n)
+Auxiliary Space: O(1)
+
+---
+
+## Binary search
+Time complexity: O(log N)
+Auxiliary Space: O(1)
+
+Array must be sorted.
+---
+
+## Interpolation search
+Time complexity: O(loglog N)
+Auxiliary Space: O(1)
+
---
+++ search/interpolation_search.c
@@ -0,0 +1,15 @@
+int interpolation_search(int *arr, int size, int target){
+    int left = 0;
+    int right = size - 1;
+    while(left <= right && target >= arr[left] && target <= arr[right]){
+        int pos = left + (target-arr[left])*(right-left)/(arr[right]-arr[left]);
+        if(arr[pos] == target){
+            return pos;
+        } else if(arr[pos] < target){
+            left = pos + 1;
+        } else{
+            right = pos - 1;
+        }
+    }
+    return -1;
+}
